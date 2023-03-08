75b5162cdf688849d8194eb4365d5d68c2583196
Sayan Samanta <samantasayan23@gmail.com>
2023-03-08 22:48:56 +0530

    ⚡️ added finishing touch

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Avatar,Card as AntCard } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Avatar,Card as AntCard, Tag } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined, DeleteOutlined } from '@ant-design/icons';
 import Meta from 'antd/es/card/Meta';
 import videoLogo from "../../assets/videoLogo.png"
 import { useDispatch } from 'react-redux';
 import { addHistory, clearCards, deleteCard, getCardItems } from '../../utils/cardSlice';
+import { collection, where, query, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 const Card = ({cardDetails, setCardDetailModalOpen, setSelectedCard, setCardEditModalOpen}) => {
     const dispatch = useDispatch()
+    const [bucketName, setBucketName] = useState("")
+
+    useEffect(async () => {
+        if (cardDetails) {
+            const bucketsRef = collection(db, "buckets");
+            const q = query(bucketsRef, where("id", "==", cardDetails.bucket));
+            const docsSnap = await getDocs(q);
+            docsSnap.forEach(doc => {
+                let bucket = doc.data()
+                setBucketName(bucket.name)
+            })
+        }
+    }, [cardDetails])
   return (
     <AntCard
         style={{ width: 300, borderTop: "5px solid black", margin: "10px 20px", cursor: "pointer" }}
@@ -39,6 +54,7 @@ const Card = ({cardDetails, setCardDetailModalOpen, setSelectedCard, setCardEdit
         }} />,
         ]}
     >
+        <Tag style={{marginBottom: "5px"}} color="magenta">{bucketName}</Tag>
         <div style={{fontWeight: 600, fontSize: "18px", height: "30px", overflow: "hidden", width: "100%", textOverflow: "ellipsis"}}>{cardDetails.name}</div>
         <div style={{fontSize: "16px", height: "30px", overflow: "hidden", width: "100%", textOverflow: "ellipsis"}}>
             {cardDetails.link}
